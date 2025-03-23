4f085e25b47a0dac2ca2fa65581f959a630c8618
Mr Punk da Silva <gustavojs417@gmail.com>
2025-03-22 22:13:30 -0300

    initialize products component

--- src/app/pages/admin/login/login.component.ts
+++ src/app/pages/admin/login/login.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  loginObj: any = {
+    username: "",
+    password: ""
+  };
+
+  constructor(private  router: Router) {
+  }
+
+  onLogin() {
+    if (this.loginObj.username == "admin" && this.loginObj.password == "334455") {
+      this.router.navigateByUrl("/products");
+    } else {
+      alert("Wrong credentials");
+    }
+  }
 
 }
 
