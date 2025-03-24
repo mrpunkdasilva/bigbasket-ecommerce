7986963f3f9ac0173b7a69327529502d19d76e96
Mr Punk da Silva <gustavojs417@gmail.com>
2025-03-24 19:29:59 -0300

    made product card

--- src/app/pages/admin/products/products.component.ts
+++ src/app/pages/admin/products/products.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
 
+  isSidePanelVisible: boolean = false;
+  productObj : any = {
+  "productId": 0,
+  "productSku": "string",
+  "productName": "string",
+  "productPrice": 0,
+  "productShortName": "string",
+  "productDescription": "string",
+  "createdDate": new Date(),
+  "deliveryTimeSpan": "string",
+  "categoryId": 2147483647,
+  "productImageUrl": "string",
+  "userId": 0
+}
+
+
+  openSidePanel() {
+    this.isSidePanelVisible = true;
+  }
+
+  closeSidePanel() {
+    this.isSidePanelVisible = false;
+  }
 }
